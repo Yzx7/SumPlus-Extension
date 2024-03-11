29ce7871e83a5981a5ee1f13bfc2ede20dd24676
Gerson Rodriguez <46801325+Yzx7@users.noreply.github.com>
2024-03-11 17:58:09 -0500

    Fix err

---
+++ assets/home-DmFbpA1P.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o}from"./index-sM0Uufkr.js";import{A as r}from"./App-ByHorWpu.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
