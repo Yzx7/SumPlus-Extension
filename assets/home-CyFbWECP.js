cd17460ea9758e647fe2e64ba6c3920704b7b414
Gerson Rodriguez <46801325+Yzx7@users.noreply.github.com>
2024-03-12 23:35:52 -0500

    version 1.2

---
+++ assets/home-CyFbWECP.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o}from"./index-OAzGvocr.js";import{A as r}from"./App-CHZz25w_.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
---
+++ assets/popup-4ubEW8Z-.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o}from"./index-OAzGvocr.js";import{A as r}from"./AppPopUp-BdsbwkTy.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
